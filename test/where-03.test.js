b677a68e7a96f70284422189a37f4a214cf4f711
kawanet <u-suke@kawa.net>
2013-06-20 04:19:16 +0900

    where() accepts $gt, $ne, $exist, $size operators, etc.

---
+++ test/where-03.test.js
@@ -0,0 +1,126 @@
+var obop = require('../');
+var sample1 = require('./data/sample1.json');
+var sample2 = require('./data/sample2.json');
+var sample3 = require('./data/sample3.json');
+var assert = require('chai').assert;
+
+describe('where-03', function() {
+  describe('sample1', tests1(sample1));
+  describe('sample2', tests2(sample2));
+  describe('sample3', tests3(sample3));
+});
+
+function wheretest(sample, where, tester, mess) {
+  mess = mess || JSON.stringify(where);
+  it(mess, function(done) {
+    var selector = obop.where(where);
+    if (tester) {
+      assert.equal(typeof selector, 'function', 'selector should be a function');
+      var actual = [].concat(sample).filter(selector);
+      var expect = [].concat(sample).filter(tester);
+      assert.deepEqual(actual, expect);
+      // console.error(actual.length);
+    } else {
+      assert.notOk(selector, 'selector should be empty');
+    }
+    done();
+  });
+}
+
+function tests1(sample) {
+  return function() {
+
+    wheretest(sample, {
+      "integral": {
+        $gt: 2000
+      }
+    }, function(item) {
+      return item.integral > 2000;
+    });
+
+    wheretest(sample, {
+      "integral": {
+        $gte: 4567
+      }
+    }, function(item) {
+      return item.integral >= 4567;
+    });
+
+    wheretest(sample, {
+      "numeric": {
+        $lt: 30
+      }
+    }, function(item) {
+      return item.numeric < 30;
+    });
+
+    wheretest(sample, {
+      "numeric": {
+        $lte: 22.22
+      }
+    }, function(item) {
+      return item.numeric <= 22.22;
+    });
+
+  };
+}
+
+function tests2(sample) {
+  return function() {
+
+    wheretest(sample, {
+      "arr": {
+        $size: 0
+      }
+    }, function(item) {
+      if (!item) return true;
+      if ('undefined' == typeof item.arr) return true;
+      if (!(item.arr instanceof Array)) return false;
+      return !item.arr.length;
+    });
+
+    wheretest(sample, {
+      "arr": {
+        $size: 1
+      }
+    }, function(item) {
+      if (!item) return false;
+      if ('undefined' == typeof item.arr) return false;
+      if (!(item.arr instanceof Array)) return true;
+      return item.arr.length == 1;
+    });
+
+    wheretest(sample, {
+      "arr": {
+        $size: 6
+      }
+    }, function(item) {
+      var arr = item.arr || [];
+      if (!(arr instanceof Array)) arr = [arr];
+      return arr.length == 6;
+    });
+  };
+}
+
+function tests3(sample) {
+  return function() {
+
+    wheretest(sample, {
+      "a.b": {
+        $exist: true
+      }
+    }, function(item) {
+      var a = item.a || {};
+      return 'undefined' !== typeof a.b;
+    });
+
+    wheretest(sample, {
+      "a.b": {
+        $exist: false
+      }
+    }, function(item) {
+      var a = item.a || {};
+      return 'undefined' === typeof a.b;
+    });
+  };
+}
